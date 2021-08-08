d4b1f8fc35a6e429584e9c456f9bd5ec7023c18d
eusatenko <usatenko_e@mail.ru>
2021-08-08 17:27:30 +0300

    added SimpleTest

---
+++ src/test/java/qa_guru_git/SimpleTest.java
@@ -0,0 +1,33 @@
+package qa_guru_git;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SimpleTest {
+
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void FindCodeJUnit5OnPageSoftAssertionTest() {
+        open("https://github.com/");
+
+        //ищем Selenide
+        $("[name=q]").setValue("Selenide").pressEnter();
+        // слева ищем и переходим в Wikis
+        $("#js-pjax-container").$(byText("Wikis")).click();
+        // ищем SoftAssertions и переходим в него
+        $(byText("SoftAssertions")).click();
+        //проверяем, что внутри есть пример кода для JUnit5
+        $("#js-repo-pjax-container").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
+
